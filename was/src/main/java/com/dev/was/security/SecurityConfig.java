9708b68fba6a3dcc7185d2c416aedfce74a38730
JongHyunParkDev <eomgr88@gmail.com>
2023-08-02 19:41:08 +0900

    logout + security error

--- was/src/main/java/com/dev/was/security/SecurityConfig.java
+++ was/src/main/java/com/dev/was/security/SecurityConfig.java
@@ -19,17 +19,25 @@ public class SecurityConfig {
         return httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
-                .cors().and()
-                .authorizeRequests()
-                .requestMatchers("/login.html").permitAll()
-                .requestMatchers("/private/**").authenticated() //private로 시작하는 uri는 로그인 필수
-                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") //admin으로 시작하는 uri는 관릴자 계정만 접근 가능
-                .anyRequest().authenticated() //나머지 uri는 모든 접근 허용
-                .and().oauth2Login()
-                .loginPage("/login.html") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
-                .defaultSuccessUrl("/") //OAuth 구글 로그인이 성공하면 이동할 uri 설정
-                .userInfoEndpoint()//로그인 완료 후 회원 정보 받기
-                .userService(oAuth2MemberService).and().and().build(); //
+                .cors()
+                .and()
+                    .authorizeHttpRequests()
+                    .requestMatchers("/login.html").permitAll()
+                    .requestMatchers("/user/**").authenticated() // user 시작하는 uri는 로그인 필수
+                    .requestMatchers("/admin/**").hasRole("ADMIN") // admin 시작하는 uri는 관리자 계정만 접근 가능
+                    .anyRequest().authenticated() //나머지 uri는 모든 접근 허용
+                .and()
+                    .logout()
+                    .logoutUrl("/user/logout") // URL mapping for logout
+                    .logoutSuccessUrl("/login.html") // Redirect to login page after successful logout
+                    .invalidateHttpSession(true) // Invalidate HTTP session after logout
+                    .clearAuthentication(true) // Clear authentication information after logout
+                    .permitAll()
+                .and()
+                    .oauth2Login()
+                    .loginPage("/login.html") //로그인이 필요한데 로그인을 하지 않았다면 이동할 uri 설정
+                    .defaultSuccessUrl("/") //OAuth 구글 로그인이 성공하면 이동할 uri 설정
+                    .userInfoEndpoint()//로그인 완료 후 회원 정보 받기
+                    .userService(oAuth2MemberService).and().and().build();
     }
-
 }
