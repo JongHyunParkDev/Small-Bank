0abc6656503085ecf3440543be42374740deb576
JongHyunParkDev <eomgr88@gmail.com>
2023-08-31 22:43:24 +0900

    log

--- was/deploy/run.cmd
+++ was/deploy/run.cmd
@@ -1,1 +1,1 @@
-C:\Users\me\.jdks\azul-18.0.2.1\bin/java -jar -Dspring.profiles.active=release C:\Users\me\Desktop\dev\SPPD\was\target/was-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=release C:\projects\run\was-0.0.1-SNAPSHOT.jar
