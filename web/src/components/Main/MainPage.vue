d07f83a22e613a2c5fc338d90fbbcdfd474591f3
JongHyunParkDev <eomgr88@gmail.com>
2023-10-03 22:43:45 +0900

    walk, coin 적용

--- web/src/components/Main/MainPage.vue
+++ web/src/components/Main/MainPage.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="three-container" ref="threeDiv">
-        
+        <div class="box-area">
+            <div class="box" @click="updateActionCoin">
+
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { defineComponent, PropType, computed, ref, toRef, Ref, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import * as THREE from 'three';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 
 const threeDiv = ref<HTMLInputElement | null>(null);
 
-let mixer, renderer, animationId = -1;
+let isCoinState = false;
+// three js 에서 typescript 모두 지원하지 않음으로 대부분을 any 로 둔다.
+let mixer: any, renderer: any, lastObject: any, animationId = -1;
 let scene = new THREE.Scene();
 const clock = new THREE.Clock();
-console.log(clock);
-console.log(scene);
+
 scene.background = new THREE.Color( 0xffffff );
 scene.fog = new THREE.Fog( 0xa0a0a0, 200, 500);
 
@@ -26,6 +31,56 @@ camera.lookAt( 0, 0, 0 );
 const ambLight = new THREE.AmbientLight( 0xffffff, 15);
 ambLight.position.set( 300, 300, 300 );
 scene.add( ambLight );
+
+function updateActionWalk() {
+    isCoinState = false;
+
+    const loader = new FBXLoader();
+    loader.load( 'src/assets/three/pig_walk.fbx', function ( object: any ) {
+        if (lastObject !== undefined) scene.remove( lastObject );
+        if (mixer !== undefined) mixer.stopAllAction();
+
+        mixer = new THREE.AnimationMixer( object );
+
+        const action = mixer.clipAction( object.animations[ 0 ] );
+
+        action.setDuration(action.getClip().duration * 0.75);
+
+        action.play();
+
+        scene.add( object );
+        lastObject = object;
+    });
+
+}
+
+function updateActionCoin() {
+    if (isCoinState) return;
+    isCoinState = true;
+
+    const loader = new FBXLoader();
+    loader.load('src/assets/three/pig_coin.fbx', function (object: any) {
+        scene.remove( lastObject );
+        mixer.stopAllAction();
+
+        mixer = new THREE.AnimationMixer( object );
+        const action = mixer.clipAction(object.animations[0]);
+
+        action.setDuration(action.getClip().duration * 0.75);
+        action.play();
+
+        // 기존 애니메이션의 종료 이벤트 리스너 추가
+        action.clampWhenFinished = true;
+        action.loop = THREE.LoopOnce; // 한 번만 재생
+        mixer.addEventListener('finished', () => {
+            // 애니메이션 종료 시 다른 애니메이션으로 전환
+            updateActionWalk();
+        });
+
+        scene.add( object );
+        lastObject = object;
+    });
+}
 
 function animate() {
     animationId = requestAnimationFrame( animate );
@@ -38,25 +93,7 @@ function animate() {
 }
 
 onMounted(() => {
-    const loader = new FBXLoader();
-    loader.load( 'src/assets/three/pig_walk.fbx', function ( object ) {
-        mixer = new THREE.AnimationMixer( object );
-
-        const action = mixer.clipAction( object.animations[ 0 ] );
-
-        action.setDuration(action.getClip().duration * 0.75);
-
-        action.play();
-
-        object.traverse( function ( child ) {
-            // console.log(child);
-            // if ( child.isMesh ) {
-            //     child.castShadow = true;
-            //     child.receiveShadow = true;
-            // }
-        } );
-        scene.add( object );
-    });
+    updateActionWalk();
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setSize( threeDiv.value?.clientWidth, threeDiv.value?.clientHeight );
@@ -71,7 +108,7 @@ onUnmounted(() => {
     scene = null;
     if (animationId !== -1) cancelAnimationFrame(animationId);
     clock.stop();
-}) 
+})
 </script>
 
 <style lang="scss">
@@ -83,5 +120,25 @@ onUnmounted(() => {
     right: 0px;
     height: 100%;
     width: 100%;
+
+    > .box-area {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        > .box {
+            width: 280px;
+            height: 25%;
+
+            &:hover {
+                cursor: pointer;
+            }
+        }
+    }
 }
 </style>
