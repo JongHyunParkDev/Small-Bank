3974fe8fb55fa5aac962a9467db6cc2670e51763
JongHyunParkDev <eomgr88@gmail.com>
2023-11-07 22:38:37 +0900

    build 후 에러로 인한 수정

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,11 +4,18 @@
 
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/AuthStore';
-
+import { useRouter } from 'vue-router';
 const authStore = useAuthStore();
+const router = useRouter();
 
 init();
 async function init () {
-    await authStore.login();
+    try {
+        await authStore.login();
+        router.push('/');
+    }
+    catch (error) {
+        router.push('/login');
+    }
 }
 </script>
